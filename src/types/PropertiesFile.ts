07ed8aeb470759783aaed2783d22fb19965f9a74
unknown <alanescarcha11@gmail.com>
2023-08-15 01:35:36 -0300

    added new types and bedrock types

--- src/types/PropertiesFile.ts
+++ src/types/PropertiesFile.ts
@@ -1,4 +1,5 @@
 export interface PropertiesFile {
+    // Java
     "broadcast-rcon-to-ops"?: boolean | string
     "view-distance"?: number | string
     "max-build-height"?: number | string
@@ -30,10 +31,10 @@ export interface PropertiesFile {
     "generate-structures"?: boolean | string
     "spawn-animals"?: boolean | string
     "snooper-enabled"?: boolean | string
-    "difficulty"?: 0 | 1 | 2 | 3 | "peaceful" | "easy" | "normal" | "hard"
+    "difficulty"?: string
     "function-permission-level"?: number | string
     "network-compression-threshold"?: number | string
-    "level-type"?: "default" | "flat" | "largebiomes" | "amplified" | "customized"
+    "level-type"?: string
     "spawn-monsters"?: boolean | string
     "max-tick-time"?: number | string
     "enforce-whitelist"?: boolean | string
@@ -44,4 +45,45 @@ export interface PropertiesFile {
     "online-mode"?: boolean | string
     "allow-flight"?: boolean | string
     "max-world-size"?: number | string
+    "enable-jmx-monitoring"?: boolean | string
+    "enforce-secure-profile"?: boolean | string
+    "max-chained-neighbor-updates"?: string
+    "require-resource-pack"?: boolean | string
+    "enable-status"?: boolean | string
+    "initial-disabled-packs"?: string
+    "resource-pack-prompt"?: string
+    "sync-chunk-writes"?: boolean | string
+    "hide-online-players"?: boolean | string
+    "entity-broadcast-range-percentage"?: number | string
+    "simulation-distance"?: number | string
+    "rate-limit"?: number | string
+    "initial-enabled-packs"?: string
+    "text-filtering-config"?: string
+    // Bedrock
+    "server-name"?: string
+    "allow-cheats"?: boolean | string
+    "allow-list"?: boolean | string
+    "server-portv6"?: number | string
+    "enable-lan-visibility"?: boolean | string
+    "tick-distance"?: number | string
+    "max-threads"?: number | string
+    "default-player-permission-level"?: string
+    "texturepack-required"?: boolean | string
+    "content-log-file-enabled"?: boolean | string
+    "compression-threshold"?: number | string
+    "compression-algorithm"?: string
+    "server-authoritative-movement"?: string
+    "player-movement-score-threshold"?: number | string
+    "player-movement-action-direction-threshold"?: number | string
+    "player-movement-distance-threshold"?: number | string
+    "player-movement-duration-threshold-in-ms"?: number | string
+    "correct-player-movement"?: boolean | string
+    "server-authoritative-block-breaking"?: boolean | string
+    "chat-restriction"?: string
+    "disable-player-interaction"?: boolean | string
+    "client-side-chunk-generation-enabled"?: boolean | string
+    "block-network-ids-are-hashes"?: boolean | string
+    "disable-persona"?: boolean | string
+    "disable-custom-skins"?: boolean | string
+    "server-build-radius-ratio"?: string
 }
